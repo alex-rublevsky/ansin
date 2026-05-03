1c2793dfb7357fa0e2f1ae9cd3358c8985e1dfee
alex-rublevsky <alexander.rublevskii@gmail.com>
2026-05-03 00:17:41 -0400

    initial set up: font, configs, basic css, tailwind, favicons, webmanifest, main layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,50 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField, fontProviders } from "astro/config";
+
+import node from "@astrojs/node";
+
+import icon from "astro-icon";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://ansin.ru",
+
+  env: {
+    schema: {
+      // TODO: update these variable examples
+      SECRET_BETTER_AUTH: envField.string({
+        context: "server",
+        access: "secret",
+      }),
+    },
+  },
+
+  adapter: node({
+    mode: "standalone",
+  }),
+
+  integrations: [icon()],
+
+  fonts: [
+    {
+      provider: fontProviders.local(),
+      name: "Overused Grotesk",
+      cssVariable: "--font-overused-grotesk",
+      options: {
+        variants: [
+          {
+            weight: "300 900",
+            style: "normal",
+            src: ["./src/assets/fonts/OverusedGrotesk.woff2"],
+          },
+        ],
+      },
+    },
+  ],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
---
+++ src/env.d.ts
@@ -0,0 +1,7 @@
+interface ImportMetaEnv {
+  readonly SECRET_BETTER_AUTH: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
